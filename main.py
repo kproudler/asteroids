da6b5afd309f18e958c4f15bcbebf62026592b6d
Kevin Proudler <kevinproudler@gmail.com>
2025-04-23 14:29:18 -0400

    added player forward/backward movement

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import pygame
 from constants import * 
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
-        pygame.display.flip()
 
-        delta = clock.tick(60)
-        delta /= 1000
+        dt = clock.tick(60)
+        dt /= 1000
+
+        player.update(dt)
+
+        player.draw(screen)
+        
+        pygame.display.flip()
         
 
 if __name__ == "__main__":
