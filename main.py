528e99d3b5291a20326ebb6e07b140ffd08fe70c
Kevin Proudler <kevinproudler@gmail.com>
2025-04-24 09:58:56 -0400

    changed to using groups for objects

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from constants import *
 from player import Player
 
 def main():
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
 
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x, y)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -22,12 +25,13 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
 
-        dt = clock.tick(60)
-        dt /= 1000
+        dt = clock.tick(60) / 1000
 
-        player.update(dt)
+        updatable.update(dt)
 
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+        
         
         pygame.display.flip()
         
